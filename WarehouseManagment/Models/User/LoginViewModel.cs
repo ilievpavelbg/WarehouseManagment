31fa448188d7f9081c884caa7976a3a79920eabd
Pavel-Iliev-1 <pvl_iliev@abv.bg>
2023-12-30 16:32:09 +0200

    Connection string modification

--- WarehouseManagment/Models/User/LoginViewModel.cs
+++ WarehouseManagment/Models/User/LoginViewModel.cs
@@ -11,8 +11,14 @@ namespace WarehouseManagment.Models.User
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
-        public string Key { get; } = "6LfHlwUpAAAAADBBBt6PPP-e4i9paDuHrzJykFpN";
-        public string Secret { get; } = "6LfHlwUpAAAAAJ_1yiFRkOYj5y3OHTclo0Ki5sK_";
+
+        // This Key and Secret are for the localhost
+        //public string Key { get; } = "6LfHlwUpAAAAADBBBt6PPP-e4i9paDuHrzJykFpN";
+        //public string Secret { get; } = "6LfHlwUpAAAAAJ_1yiFRkOYj5y3OHTclo0Ki5sK_";
+
+        // This Key and Secret are for the AWS
+        public string Key { get; } = "6LeiBEApAAAAAAiHyWz89NlUXVW15YxAGGJbKPl2";
+        public string Secret { get; } = "6LeiBEApAAAAANpTRmRtlnBJ7j2CLH8eGdWGILH9";
         public string Response { get; set; } = string.Empty;
         public bool Success { get; set; }
     }
