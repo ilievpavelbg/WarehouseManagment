5aedbce33c47134b41d6689307e18865e71a35e1
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:27:55 +0300

    Add warehouse MVC model

---
+++ WarehouseManagment/Models/WarehouseModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WarehouseManagment.Data;
+
+namespace WarehouseManagment.Models
+{
+    public class WarehouseModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string Code { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public List<WarehouseZone> Zones { get; set; } = new List<WarehouseZone>();
+        public List<WarehouseLocation> Locations { get; set; } = new List<WarehouseLocation>();
+    }
+}
