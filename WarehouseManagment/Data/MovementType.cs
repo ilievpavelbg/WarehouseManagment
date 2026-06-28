c3a4bd33a9383a101f928d2acacdba601beab3f7
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:25:14 +0300

    Add WMS movement type enum

---
+++ WarehouseManagment/Data/MovementType.cs
@@ -0,0 +1,14 @@
+﻿namespace WarehouseManagment.Data
+{
+    public enum MovementType
+    {
+        Receipt = 1,
+        Issue = 2,
+        AdjustmentIncrease = 3,
+        AdjustmentDecrease = 4,
+        Transfer = 5,
+        Sale = 6,
+        CourierShipment = 7,
+        Return = 8
+    }
+}
