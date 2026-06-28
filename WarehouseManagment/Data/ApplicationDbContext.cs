cf7b7b58712a59ef99d36ac2ff84c6c8149cfc7d
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:27:10 +0300

    Register WMS warehouse and movement entities

--- WarehouseManagment/Data/ApplicationDbContext.cs
+++ WarehouseManagment/Data/ApplicationDbContext.cs
@@ -15,6 +15,10 @@ namespace WarehouseManagment.Data
         public DbSet<Sale> Sales { get; set; }
         public DbSet<Courier> Couriers { get; set; }
         public DbSet<LoginHistory> LoginHistories { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<WarehouseZone> WarehouseZones { get; set; }
+        public DbSet<WarehouseLocation> WarehouseLocations { get; set; }
+        public DbSet<InventoryMovement> InventoryMovements { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -23,6 +27,99 @@ namespace WarehouseManagment.Data
             builder.Entity<Product>()
                 .HasIndex(p => p.SKU)
                 .IsUnique();
+
+            builder.Entity<Warehouse>()
+                .HasIndex(w => w.Code)
+                .IsUnique();
+
+            builder.Entity<WarehouseZone>()
+                .HasIndex(z => new { z.WarehouseId, z.Code })
+                .IsUnique();
+
+            builder.Entity<WarehouseLocation>()
+                .HasIndex(l => new { l.WarehouseId, l.Code })
+                .IsUnique();
+
+            builder.Entity<WarehouseZone>()
+                .HasOne(z => z.Warehouse)
+                .WithMany(w => w.Zones)
+                .HasForeignKey(z => z.WarehouseId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<WarehouseLocation>()
+                .HasOne(l => l.Warehouse)
+                .WithMany(w => w.Locations)
+                .HasForeignKey(l => l.WarehouseId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<WarehouseLocation>()
+                .HasOne(l => l.WarehouseZone)
+                .WithMany(z => z.Locations)
+                .HasForeignKey(l => l.WarehouseZoneId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasIndex(m => m.MovementDate);
+
+            builder.Entity<InventoryMovement>()
+                .HasIndex(m => m.ProductId);
+
+            builder.Entity<InventoryMovement>()
+                .HasIndex(m => m.ProductInventoryId);
+
+            builder.Entity<InventoryMovement>()
+                .HasIndex(m => m.WarehouseId);
+
+            builder.Entity<InventoryMovement>()
+                .HasIndex(m => m.WarehouseLocationId);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.Product)
+                .WithMany()
+                .HasForeignKey(m => m.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.ProductInventory)
+                .WithMany()
+                .HasForeignKey(m => m.ProductInventoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.Warehouse)
+                .WithMany(w => w.InventoryMovements)
+                .HasForeignKey(m => m.WarehouseId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.WarehouseZone)
+                .WithMany(z => z.InventoryMovements)
+                .HasForeignKey(m => m.WarehouseZoneId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.WarehouseLocation)
+                .WithMany(l => l.InventoryMovements)
+                .HasForeignKey(m => m.WarehouseLocationId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.DestinationWarehouse)
+                .WithMany()
+                .HasForeignKey(m => m.DestinationWarehouseId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.DestinationWarehouseZone)
+                .WithMany()
+                .HasForeignKey(m => m.DestinationWarehouseZoneId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<InventoryMovement>()
+                .HasOne(m => m.DestinationWarehouseLocation)
+                .WithMany()
+                .HasForeignKey(m => m.DestinationWarehouseLocationId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
