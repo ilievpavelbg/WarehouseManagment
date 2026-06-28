34d3964ebfc9c0a90783f1cd0d84ba3fe3adf1b2
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:25:27 +0300

    Add WMS stock item type enum

---
+++ WarehouseManagment/Data/StockItemType.cs
@@ -0,0 +1,11 @@
+﻿namespace WarehouseManagment.Data
+{
+    public enum StockItemType
+    {
+        RawMaterial = 1,
+        PurchasedMaterial = 2,
+        WorkInProgress = 3,
+        FinishedGood = 4,
+        Product = 5
+    }
+}
