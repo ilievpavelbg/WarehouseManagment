8224c3a93ba27393169e14d284a32f60a764cf69
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:26:22 +0300

    Add inventory movement ledger entity

---
+++ WarehouseManagment/Data/InventoryMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WarehouseManagment.Data
+{
+    public class InventoryMovement
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public MovementType MovementType { get; set; }
+
+        [Required]
+        public StockItemType StockItemType { get; set; }
+
+        public int? ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        public int? ProductInventoryId { get; set; }
+        public ProductInventory? ProductInventory { get; set; }
+
+        [Required]
+        [Range(0.0001, double.MaxValue)]
+        public decimal Quantity { get; set; }
+
+        public DateTime MovementDate { get; set; } = DateTime.Now;
+
+        public int? WarehouseId { get; set; }
+        public Warehouse? Warehouse { get; set; }
+
+        public int? WarehouseZoneId { get; set; }
+        public WarehouseZone? WarehouseZone { get; set; }
+
+        public int? WarehouseLocationId { get; set; }
+        public WarehouseLocation? WarehouseLocation { get; set; }
+
+        public int? DestinationWarehouseId { get; set; }
+        public Warehouse? DestinationWarehouse { get; set; }
+
+        public int? DestinationWarehouseZoneId { get; set; }
+        public WarehouseZone? DestinationWarehouseZone { get; set; }
+
+        public int? DestinationWarehouseLocationId { get; set; }
+        public WarehouseLocation? DestinationWarehouseLocation { get; set; }
+
+        [StringLength(100)]
+        public string? ReferenceNumber { get; set; }
+
+        [StringLength(100)]
+        public string? BatchNumber { get; set; }
+
+        [StringLength(100)]
+        public string? LotNumber { get; set; }
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+
+        [StringLength(450)]
+        public string? UserId { get; set; }
+    }
+}
