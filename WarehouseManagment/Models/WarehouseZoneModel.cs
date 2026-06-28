5841e9b19c5ff3e1645999dcf8f0ee2cd9681012
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:28:07 +0300

    Add warehouse zone MVC model

---
+++ WarehouseManagment/Models/WarehouseZoneModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseManagment.Models
+{
+    public class WarehouseZoneModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string Code { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        [Range(1, int.MaxValue)]
+        public int WarehouseId { get; set; }
+    }
+}
