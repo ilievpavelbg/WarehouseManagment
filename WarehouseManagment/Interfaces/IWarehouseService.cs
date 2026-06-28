ccc507c066c613fff0dcd23a5f374966d57676ef
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:28:14 +0300

    Add warehouse service interface

---
+++ WarehouseManagment/Interfaces/IWarehouseService.cs
@@ -0,0 +1,14 @@
+﻿using WarehouseManagment.Data;
+using WarehouseManagment.Models;
+
+namespace WarehouseManagment.Interfaces
+{
+    public interface IWarehouseService
+    {
+        Task<List<Warehouse>> GetAllWarehousesAsync();
+        Task<Warehouse> GetWarehouseByIdAsync(int id);
+        Task CreateWarehouseAsync(WarehouseModel model);
+        Task CreateZoneAsync(WarehouseZoneModel model);
+        Task CreateLocationAsync(WarehouseLocationModel model);
+    }
+}
