8de4bcc749ed3ad8ce9cbfe11ce3e67d34bac3b3
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-28 16:26:02 +0300

    Add warehouse zone entity

---
+++ WarehouseManagment/Data/WarehouseZone.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WarehouseManagment.Data
+{
+    public class WarehouseZone
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string Code { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        [ForeignKey(nameof(Warehouse))]
+        public int WarehouseId { get; set; }
+        public Warehouse Warehouse { get; set; } = null!;
+
+        public List<WarehouseLocation> Locations { get; set; } = new List<WarehouseLocation>();
+        public List<InventoryMovement> InventoryMovements { get; set; } = new List<InventoryMovement>();
+    }
+}
