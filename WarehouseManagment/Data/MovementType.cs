38105404ea1f8f4f1dba91a10728caef8a4aacd2
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-29 16:00:37 +0300

    Update inventory movement types

--- WarehouseManagment/Data/MovementType.cs
+++ WarehouseManagment/Data/MovementType.cs
@@ -2,13 +2,15 @@
 {
     public enum MovementType
     {
-        Receipt = 1,
-        Issue = 2,
-        AdjustmentIncrease = 3,
-        AdjustmentDecrease = 4,
-        Transfer = 5,
-        Sale = 6,
-        CourierShipment = 7,
-        Return = 8
+        ImportReceipt = 1,
+        Sale = 2,
+        SaleReversal = 3,
+        CourierShipment = 4,
+        CourierReversal = 5,
+        Adjustment = 6,
+        ProductionConsumption = 7,
+        ProductionOutput = 8,
+        Transfer = 9,
+        Return = 10
     }
 }
