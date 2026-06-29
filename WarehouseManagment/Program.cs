8792b89edd989dce0fdcc72fc194008e579ef0c5
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-29 16:01:12 +0300

    Register inventory movement service

--- WarehouseManagment/Program.cs
+++ WarehouseManagment/Program.cs
@@ -44,6 +44,7 @@ builder.Services.AddScoped<ISaleService, SaleService>();
 builder.Services.AddScoped<ICourierService, CourierService>();
 builder.Services.AddScoped<ILoginHistoryService, LoginHistoryService>();
 builder.Services.AddScoped<IWarehouseService, WarehouseService>();
+builder.Services.AddScoped<IInventoryMovementService, InventoryMovementService>();
 builder.Services.AddSingleton<IHttpContextAccessor,  HttpContextAccessor>();
 
 var app = builder.Build();
