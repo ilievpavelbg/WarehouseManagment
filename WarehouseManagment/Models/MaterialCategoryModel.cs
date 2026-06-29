cac6b914506d6a841447bd4c5fc54f07990edad0
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-29 16:32:32 +0300

    Add material category form model

---
+++ WarehouseManagment/Models/MaterialCategoryModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseManagment.Models
+{
+    public class MaterialCategoryModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string Code { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
