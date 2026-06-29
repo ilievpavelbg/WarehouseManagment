59a9a6315e9302ed78236b1e4244c6ac27040f53
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-29 16:00:07 +0300

    Add inventory movement service interface

---
+++ WarehouseManagment/Interfaces/IInventoryMovementService.cs
@@ -0,0 +1,9 @@
+﻿using WarehouseManagment.Models;
+
+namespace WarehouseManagment.Interfaces
+{
+    public interface IInventoryMovementService
+    {
+        Task CreateMovementAsync(InventoryMovementModel model);
+    }
+}
