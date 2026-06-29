630dbe37569c883f540f9da092fbfcbb23f0454e
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-29 15:59:55 +0300

    Add inventory movement request model

---
+++ WarehouseManagment/Models/InventoryMovementModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WarehouseManagment.Data;
+
+namespace WarehouseManagment.Models
+{
+    public class InventoryMovementModel
+    {
+        [Range(1, int.MaxValue)]
+        public int ProductInventoryId { get; set; }
+
+        public int? WarehouseId { get; set; }
+
+        public int? WarehouseLocationId { get; set; }
+
+        [Required]
+        public MovementType MovementType { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ReferenceType { get; set; } = null!;
+
+        [Range(1, long.MaxValue)]
+        public long ReferenceId { get; set; }
+
+        [StringLength(450)]
+        public string? UserId { get; set; }
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+    }
+}
