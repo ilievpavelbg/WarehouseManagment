bb5ab9826ae0744a3a2f7998285ce207997e7257
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-06-30 19:07:11 +0300

    Add material stock change model

---
+++ WarehouseManagment/Models/MaterialStockChangeModel.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using WarehouseManagment.Data;
+
+namespace WarehouseManagment.Models
+{
+    public class MaterialStockChangeModel
+    {
+        [Range(1, int.MaxValue)]
+        public int MaterialId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int WarehouseId { get; set; }
+
+        public int? WarehouseLocationId { get; set; }
+
+        public int? MaterialBatchId { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        [Required]
+        public MovementType MovementType { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ReferenceType { get; set; } = null!;
+
+        [Range(typeof(long), "1", "9223372036854775807")]
+        public long ReferenceId { get; set; }
+
+        [StringLength(450)]
+        public string? UserId { get; set; }
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+    }
+}
