df5e16969f912261bdfa1ce3b0c98bcd6a8658a0
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-07-16 10:42:57 +0300

    Add material stock card warehouse model

---
+++ WarehouseManagment/Models/MaterialStockCardWarehouseModel.cs
@@ -0,0 +1,9 @@
+namespace WarehouseManagment.Models
+{
+    public class MaterialStockCardWarehouseModel
+    {
+        public string WarehouseName { get; set; } = string.Empty;
+
+        public decimal Quantity { get; set; }
+    }
+}
