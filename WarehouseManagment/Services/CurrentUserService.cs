cb350b7d5913b0f10cceb5cddf15b98a5a29c53d
Pavel Iliev <93718688+ilievpavelbg@users.noreply.github.com>
2026-07-17 15:31:18 +0300

    Add current user service

---
+++ WarehouseManagment/Services/CurrentUserService.cs
@@ -0,0 +1,21 @@
+using System.Security.Claims;
+using WarehouseManagment.Interfaces;
+
+namespace WarehouseManagment.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        public string? UserName => _httpContextAccessor.HttpContext?.User?.Identity?.Name;
+
+        public string? IpAddress => _httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString();
+    }
+}
